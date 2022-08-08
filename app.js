b73613af7a1bd62354015763a12985a45df6644e
Henrik Niskanen <henrik.niskanen@gambitgroup.fi>
2022-08-08 16:18:45 +0300

    fix no-console lint issue

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const express = require('express')
 const app = express()
 
@@ -7,6 +8,5 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  // eslint-disable no-console
   console.log('server started on port 5000')
 })
